a7fe0a3185312a9b18c055a0d19e0aae99d886dd
Nick Ubuntu <nickmohan12@gmail.com>
2019-03-09 14:43:13 -0500

    Fixes

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from twilio.twiml.messaging_response import MessagingResponse, Message
-from twilio.rest import Clien
+from twilio.rest import Client
 
 client = Client('AC1519f6c35abae292854d60746394de52', '48e3893e7ad7e865b6893ba175160fe4')
 app = Flask(__name__)
