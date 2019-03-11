3dccf86a9ec249428f462cf5716cf0b3e010d241
Steven Shan <steven.shan99@gmail.com>
2019-03-11 13:02:36 -0400

    android app

--- app/app/src/main/java/com/example/app/ScrollingActivity.java
+++ app/app/src/main/java/com/example/app/ScrollingActivity.java
@@ -5,11 +5,48 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.view.View.OnClickListener;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.widget.ImageView;
+import android.view.KeyEvent;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 
 public class ScrollingActivity extends AppCompatActivity {
+    final SmsManager sms = SmsManager.getDefault();
+
+    private void registerSmsListener() {
+        ImageView image_view =(ImageView)findViewById(R.id.mainImage);
+        image_view.setClickable(true);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        /* filter.setPriority(999); This is optional. */
+        EditText edittext = (EditText) findViewById(R.id.searchBar);
+        IncomingSms receiver = new IncomingSms(image_view, edittext);
+        registerReceiver(receiver, filter);
+    }
+
+    private void requestPermission(String permission) {
+        int grant = ContextCompat.checkSelfPermission(this, permission);
+        if ( grant != PackageManager.PERMISSION_GRANTED) {
+            String[] permission_list = new String[1];
+            permission_list[0] = permission;
+            ActivityCompat.requestPermissions(this, permission_list, 1);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +65,27 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
         */
+        /* Register the broadcast receiver */
+        registerSmsListener();
+
+        /* Make sure, we have the permissions */
+        requestPermission(Manifest.permission.SEND_SMS);
+        requestPermission(Manifest.permission.RECEIVE_SMS);
+        requestPermission(Manifest.permission.READ_SMS);
+
+        final EditText edittext = (EditText) findViewById(R.id.searchBar);
+        edittext.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    sms.sendTextMessage("5558", null, edittext.getText().toString(), null, null);
+                    // Perform action on key press
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
